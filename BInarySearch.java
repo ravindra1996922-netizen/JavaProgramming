7f62199ef866789b02e67461049e7c82ed98506e
maverick <ravindra1996922@gmail.com>
2026-02-11 20:54:19 +0530

    binary search commit 1st time

---
+++ BInarySearch.java
@@ -0,0 +1,35 @@
+public class BInarySearch {
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2; 
+
+            if (arr[mid] == target) {
+                return mid; 
+            } 
+            else if (arr[mid] < target) {
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1; 
+            }
+        }
+
+        return -1; 
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91}; 
+        int target = 23;
+
+        int result = binarySearch(arr, target);
+
+        if (result == -1) {
+            System.out.println("Not found");
+        } else {
+            System.out.println("Found at index: " + result);
+        }
+    }
+}
